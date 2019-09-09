4f3dfff0e16e62d55b7b3315e194a9ce83c94a38
Connor Cox <10873383@my.uvu.edu>
2019-09-09 15:14:04 -0600

    Added Scaffolding to windmill

---
+++ Unity/Hyper Casual/Assets/Scripts/ImageController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+//[RequireComponent(typeof(Image))]
+//public class ImageController : MonoBehaviour
+//{
+//    private Image imageComponent;
+//    public UnityEvent UpdateImageEvent;
+//    Private void Start()
+//    {
+//        imageComponent = GetComponent<Image>();
+//    }
+//    
+//}
