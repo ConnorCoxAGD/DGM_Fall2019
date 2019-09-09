a6bcbbd755dff73cafabf2f7678e1b8ab08184a7
Connor (Home) <10873383@my.uvu.edu>
2019-09-08 21:25:36 -0600

    Soft Rotation Script
    Wrote a script to rotate an object with acceleration and deceleration.

---
+++ Unity/Hyper Casual/Assets/Scripts/RotateGameObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class RotateGameObject : MonoBehaviour
+{
+    public float RotationSpeed;
+    private float _increment = 2f;
+    private float _maxSpeed = 1f;
+
+    private void Update()
+    {
+        RotateTransform();
+    }
+    
+    private void RotateTransform()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            transform.Rotate(0f, -RotationSpeed, 0f);
+        }
+
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            transform.Rotate(0f, RotationSpeed, 0f);
+        }
+        
+        if (Input.GetAxisRaw("Horizontal") < 0 || (Input.GetAxisRaw("Horizontal") > 0))
+        {
+            RotationSpeed += _increment * Time.deltaTime;
+        }
+        else
+        {
+            RotationSpeed -= _increment * 2 * Time.deltaTime;
+        }
+        RotationSpeed = Mathf.Clamp (RotationSpeed, .3f, _maxSpeed);
+    }
+
+
+}
