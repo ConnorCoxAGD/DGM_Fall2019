257e9b7b8d9623a1779d59e8e7ec18ad86213587
Connor Cox <10873383@my.uvu.edu>
2019-10-21 16:32:09 -0600

    Generating better trees in Houdini

---
+++ Unity/Hyper Casual/Assets/Scripts/EventListening.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventListening : MonoBehaviour, IListen
+{
+    public Object IRunObj;
+    public UnityEvent Event { get; set; }
+    public IRun NewIRunObj { get; set; }
+
+    public void Start()
+    {
+        NewIRunObj = IRunObj as IRun;
+        Event.AddListener(NewIrunObj.Run);
+    }
+}
---
+++ Unity/Hyper Casual/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+[CreateAssetMenu]
+public class PowerUp : ScriptableObject, IRun
+{
+   public void Run()
+   {
+      Debug.Log("PowerUp");
+   }
+}
