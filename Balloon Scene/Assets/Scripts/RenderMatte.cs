78af7eab0fba2503b2fd30e3db549fe99930a8fc
Connor Cox <10873383@my.uvu.edu>
2019-11-06 16:01:13 -0700

    New project to render Balloon Scene

---
+++ Balloon Scene/Assets/Scripts/RenderMatte.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderMatte : MonoBehaviour
+{
+    public BoolData useMatte;
+    public Material materialObj;
+
+    private void Start()
+    {
+        if (useMatte.value)
+        {
+            GetComponent<Renderer>().material = materialObj;
+        }
+    }
+}
