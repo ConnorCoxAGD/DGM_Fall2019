46b6610175e9fa03d66641b98ac6f5c2b2472aee
Connor (Home) <10873383@my.uvu.edu>
2019-11-05 23:54:32 -0700

    ID Trigger Added

---
+++ Unity/Hyper Casual/Assets/Scripts/ClassIDTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+public class ClassIDTrigger : MonoBehaviour
+{
+    public ClassID TargetID;
+    public UnityEvent EnterEvent, StayEvent, ExitEvent;
+
+    private void OnTriggerEnter(Collider obj)
+    {
+        if (obj.GetComponent<ClassID>().objID == TargetID)
+        {
+            EnterEvent.Invoke();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (GetComponent<ClassID>().objID == TargetID)
+    }
+}
