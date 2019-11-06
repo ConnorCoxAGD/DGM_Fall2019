4b4bc4c1cc2d0fa570df221d6170088a7083eeb0
Connor (Home) <10873383@my.uvu.edu>
2019-11-06 00:51:51 -0700

    List script that adds and removes on trigger

---
+++ Unity/Hyper Casual/Assets/Scripts/TargetArray.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetArray : MonoBehaviour
+{
+    public NameID SpecifiedID;
+    public List<GameObject> targets = new List<GameObject>();
+
+    private void OnTriggerEnter(Collider obj)
+    {
+        if (obj.GetComponent<ObjectID>().ID == SpecifiedID)
+        {
+            targets.Add(obj.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider obj)
+    {
+        targets.Remove(obj.gameObject);
+    }
+}
---
+++ Unity/Hyper Casual/Assets/Scripts/TriggerID.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerID : MonoBehaviour
+{
+    public NameID SpecifiedID;
+    public UnityEvent EnterEvent, StayEvent, ExitEvent;
+
+    private void OnTriggerEnter(Collider obj)
+    {
+        if (obj.GetComponent<ObjectID>().ID == SpecifiedID)
+        {
+            EnterEvent.Invoke();
+        }
+    }
+
+    private void OnTriggerStay(Collider obj)
+    {
+        if (obj.GetComponent<ObjectID>().ID == SpecifiedID)
+        {
+            StayEvent.Invoke();
+        }
+    }
+
+    private void OnTriggerExit(Collider obj)
+    {
+        if (obj.GetComponent<ObjectID>().ID == SpecifiedID)
+        {
+            ExitEvent.Invoke();
+        }
+    }
+}
