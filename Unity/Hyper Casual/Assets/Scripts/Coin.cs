3761d628825175791e369ee46ba0ba2bf820752d
Connor Cox <10873383@my.uvu.edu>
2019-11-13 14:53:26 -0700

    Coin Collection Script

---
+++ Unity/Hyper Casual/Assets/Scripts/Coin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class Coin : ScriptableObject
+{
+    public UnityAction<int> coinAction;
+
+    public int value = 10;
+
+    public void Spend()
+    {
+        coinAction?.Invoke(value);
+    }
+    
+}
